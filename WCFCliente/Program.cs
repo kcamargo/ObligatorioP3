b780a56f2f79832a721e86f0d0326b149ef6c465
Kari <camargo.kari@gmail.com>
2016-05-04 12:10:27 -0300

    Ultima modificacion

---
+++ WCFCliente/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCFCliente.ServiceReference1;
+
+namespace WCFCliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.ListarAnunciosClient cliente = new ServiceReference1.ListarAnunciosClient();
+            cliente.Open();
+            Anuncio[] anuncios = cliente.GetAll();
+            foreach (Anuncio a in anuncios)
+            {
+                Console.WriteLine(a.Direccion);
+                
+            }
+            cliente.Close();
+            Console.ReadKey();
+        }
+    }
+}
